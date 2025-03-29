3c0e8cb4f5177af479bf42ff21afd063bbeb3f80
nyan <154904320+nyan-hussein@users.noreply.github.com>
2025-03-29 03:59:03 +0300

    expose script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function openModal(id) {
   img.onload = function () {
     EXIF.getData(img, function () {
       let metadata = EXIF.getAllTags(this);
-      const Exposure = (metadata.ExposureTime) ? metadata.ExposureTime.toFixed(3) : "N/A";
+      const Exposure = metadata.ExposureTime ? `1/${Math.round(1 / metadata.ExposureTime)}`: "N/A";
       let infoText = `
               Camera: ${metadata.Make || "Unknown"} ${metadata.Model || ""}
               <br> Exposure: ${Exposure} sec
